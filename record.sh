34f6bfa833b54bda393f88a79ff2de9252d09542
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 12:06:54 +0100

    Remove hidden files from tree

--- record.sh
+++ record.sh
@@ -2,7 +2,7 @@
     echo "watch --color -tn1 \"pwd\"" | asciinema rec $1-pwd.cast 
 } &
 {
-    echo "watch --color -tn1 \"tree -a\"" | asciinema rec $1-tree.cast 
+    echo "watch --color -tn1 \"tree\"" | asciinema rec $1-tree.cast 
 } &
 {
     echo "watch --color -tn1 \"git status\"" | asciinema rec $1-git-status.cast 
