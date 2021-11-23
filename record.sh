603c51bcd6c5c670dd26d41344a8a97810b10f7f
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 14:19:24 +0100

    Silent execution of background channels

--- record.sh
+++ record.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-asciinema rec -c 'watch --color -tn1 "pwd"' $1-pwd.cast &
+asciinema rec -c 'watch --color -tn1 "pwd"' --overwrite $1-pwd.cast > /dev/null &
 T1=${!}
-asciinema rec -c 'watch --color -tn1 "tree"' $1-tree.cast &
+asciinema rec -c 'watch --color -tn1 "tree"' --overwrite $1-tree.cast > /dev/null  &
 T2=${!}
-asciinema rec -c 'watch --color -tn1 "git status"' $1-git-status.cast &
+asciinema rec -c 'watch --color -tn1 "git status"' --overwrite $1-git-status.cast > /dev/null  &
 T3=${!}
-asciinema rec -c 'watch --color -tn1 "git log --oneline"' $1-git-log.cast & 
+asciinema rec -c 'watch --color -tn1 "git log --oneline"' --overwrite $1-git-log.cast > /dev/null  & 
 T4=${!}
 asciinema rec --stdin $1-terminal.cast
 kill $T1 $T2 $T3 $T4
