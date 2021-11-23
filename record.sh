db415de3dc28bff78229cf09d43fe4c67c24b603
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 11:56:37 +0100

    Add color to watch

--- record.sh
+++ record.sh
@@ -1,12 +1,12 @@
 {
-    echo "watch -tn1 pwd" | asciinema rec $1-pwd.cast 
+    echo "watch --color -tn1 \"pwd\"" | asciinema rec $1-pwd.cast 
 } &
 {
-    echo "watch -tn1 tree -a" | asciinema rec $1-tree.cast 
+    echo "watch --color -tn1 \"tree -a\"" | asciinema rec $1-tree.cast 
 } &
 {
-    echo "watch -tn1 git status" | asciinema rec $1-git-status.cast 
+    echo "watch --color -tn1 \"git status\"" | asciinema rec $1-git-status.cast 
 } &
 {
-    echo "watch -tn1 git log --oneline" | asciinema rec $1-git-log.cast 
+    echo "watch --color -tn1 \"git log --oneline\"" | asciinema rec $1-git-log.cast 
 } &
