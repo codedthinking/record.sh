8f02a37d28e23cbcd5dfc20f1e799df1887e0681
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 11:51:38 +0100

    Add git log to record

--- record.sh
+++ record.sh
@@ -2,8 +2,11 @@
     echo "watch -tn1 pwd" | asciinema rec $1-pwd.cast 
 } &
 {
-    echo "watch -tn1 tree" | asciinema rec $1-tree.cast 
+    echo "watch -tn1 tree -a" | asciinema rec $1-tree.cast 
 } &
 {
-    echo "watch -tn1 git status" | asciinema rec $1-git.cast 
+    echo "watch -tn1 git status" | asciinema rec $1-git-status.cast 
+} &
+{
+    echo "watch -tn1 git log --oneline" | asciinema rec $1-git-log.cast 
 } &
