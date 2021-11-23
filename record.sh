d826c5309ae8a5fd43a9d2c647c846142172b9de
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 14:11:12 +0100

    Add terminal channel

--- record.sh
+++ record.sh
@@ -1,12 +1,6 @@
-{
-    echo "watch --color -tn1 \"pwd\"" | asciinema rec $1-pwd.cast 
-} &
-{
-    echo "watch --color -tn1 \"tree\"" | asciinema rec $1-tree.cast 
-} &
-{
-    echo "watch --color -tn1 \"git status\"" | asciinema rec $1-git-status.cast 
-} &
-{
-    echo "watch --color -tn1 \"git log --oneline\"" | asciinema rec $1-git-log.cast 
-} &
+asciinema rec -c 'watch --color -tn1 "pwd"' $1-pwd.cast &
+asciinema rec -c 'watch --color -tn1 "tree"' $1-tree.cast &
+asciinema rec -c 'watch --color -tn1 "git status"' $1-git-status.cast &
+asciinema rec -c 'watch --color -tn1 "git log --oneline"' $1-git-log.cast & 
+asciinema rec --stdin $1-terminal.cast
+wait
