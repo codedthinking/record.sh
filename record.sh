0b191a7e9a480b52b1a49fe089bd7e2d927ed121
Miklos Koren <miklos.koren@gmail.com>
2021-11-23 14:14:59 +0100

    Automatically kill all background processes after finishing

--- record.sh
+++ record.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 asciinema rec -c 'watch --color -tn1 "pwd"' $1-pwd.cast &
+T1=${!}
 asciinema rec -c 'watch --color -tn1 "tree"' $1-tree.cast &
+T2=${!}
 asciinema rec -c 'watch --color -tn1 "git status"' $1-git-status.cast &
+T3=${!}
 asciinema rec -c 'watch --color -tn1 "git log --oneline"' $1-git-log.cast & 
+T4=${!}
 asciinema rec --stdin $1-terminal.cast
-wait
+kill $T1 $T2 $T3 $T4
